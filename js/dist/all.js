1df6280c9845976bb6b2679bd392c8aea2b27860
Tarik <tarikmerrylees@gmail.com>
2021-03-21 12:59:35 +0000

    pre owl commit

--- js/dist/all.js
+++ js/dist/all.js
@@ -7,35 +7,37 @@
     const carouselle_container = d.getElementById('carouselle_container');
 
     // get visible articles
-    let left = carouselle_container.children[0];
-    let right = carouselle_container.children[1];
+    let currentLeft = 0;
+    let currentRight = 1;
+    let left = carouselle_container.children[currentLeft];
+    let right = carouselle_container.children[currentRight];
 
     
     // hide other articles
     let hide = () => {
-        console.log("hello");
         let current = 2;
-
+        console.log("yo");
+        
         while (current <= carouselle_container.children.length-1) {
             carouselle_container.children[current].style.display = "none";
             current +=1;
         }
     }
-    hide();
-
-
-
-
-
-
-
-
-
+    // hide();
 
     // cycling backwards
-    // let prev = () => {
+    let prev = () => {
+        // hide left
+        left.style.display = "none";
 
-    // };
+        // show next child. if right is last, show first
+
+
+        
+    };
+
+
+    carouselle_prev.addEventListener("click", prev);
 
 })(document)
 ((d) => {
