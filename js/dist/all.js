f03ec9f78c5d4c8ab8832aa90bf53cdc4aab2621
Tarik <tarikmerrylees@gmail.com>
2021-03-21 20:51:28 +0000

    reactive carouselle finished

--- js/dist/all.js
+++ js/dist/all.js
@@ -1,11 +1,20 @@
 ((d) => {
     // get elements
     const carouselle_container = d.getElementById('carouselle_container');
+
     const carouselle_prev = d.getElementById('carouselle_prev');
     const carouselle_next = d.getElementById('carouselle_next');
 
+    const carouselle_prevMobile = d.getElementById('carouselle_prev-mobile');
+    const carouselle_nextMobile = d.getElementById('carouselle_next-mobile');
+
+
+    // DESKTOP -----------------------------
+    // 
+    // 
+    // 
     // when loaded only show first two children of carouselle container
-    let hide = () => {
+    const hide = () => {
         for (let i = 2; i < carouselle_container.children.length; i += 1) {
             carouselle_container.children[i].style.display = "none";
         }
@@ -13,8 +22,7 @@
 
     hide();
 
-
-    let next = () => {
+    const next = () => {
         // save it
         let first = carouselle_container.children[0];
     
@@ -48,8 +56,46 @@
         carouselle_container.children[1].style.display = "block";
     }
 
+    // MOBILE -----------------------------
+    // 
+    // 
+    // 
+    // let hideMobile = () => {
+    //     for (let i = 1; i < carouselle_container.children.length; i += 1) {
+    //         carouselle_container.children[i].style.display = "none";
+    //     }
+    // };
+    let current = 0;
+
+    const nextMobile = () => {
+        // hide current
+        carouselle_container.children[current].style.display = "none";
+
+        // update current
+        current < carouselle_container.children.length - 1 ? current += 1 : current = 0;
+        console.log(current);
+
+        // show new current
+        carouselle_container.children[current].style.display = "block";
+    };
+
+    const prevMobile = () => {
+        // hide current
+        carouselle_container.children[current].style.display = "none";
+
+        // update current
+        current > 0 ? current -= 1 : current = carouselle_container.children.length - 1;
+        console.log(current);
+
+        // show new current
+        carouselle_container.children[current].style.display = "block";
+    }
+
     carouselle_next.addEventListener("click", next);
     carouselle_prev.addEventListener("click", prev);
+
+    carouselle_nextMobile.addEventListener("click", nextMobile);
+    carouselle_prevMobile.addEventListener("click", prevMobile);
 
 })(document);
 ((d) => {
